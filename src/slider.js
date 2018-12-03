9b35d1448410f1b28a3e36c5fc8416a324910733
jessbennett <jessbennett924@gmail.com>
2018-12-03 13:17:08 -0500

    added additional commments

--- src/slider.js
+++ src/slider.js
@@ -7,7 +7,14 @@ class Slider extends Component {
     super(props)
 //TODO1: state is an object and you're expecting these objects to change
 //TODO2: Let's add 4 objects with a key and a value to our this.state object
-    this.state = {}
+//TODO3: We are setting a few of the values with an empty string because we are expecting to recieve data
+    this.state = {
+      httpData: '',
+      currentIndexInArray: 0, //TIP: we set the intial index to 0 because that's how we count the begining of an array
+      imageUrl: '',
+      imageContent: ''
+    }
+    this.onclick = this.onclick.bind(this);
   }
 
   render() {

749fe6e542c4525154c33af48dd5a12156565c3e
jessbennett <jessbennett924@gmail.com>
2018-12-03 13:12:15 -0500

    added TODO 1 and 2

--- src/slider.js
+++ src/slider.js
@@ -5,7 +5,8 @@ import axios from 'axios'
 class Slider extends Component {
   constructor(props) {
     super(props)
-
+//TODO1: state is an object and you're expecting these objects to change
+//TODO2: Let's add 4 objects with a key and a value to our this.state object
     this.state = {}
   }
 
